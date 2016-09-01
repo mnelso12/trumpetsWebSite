18957fe12e0f9351c5c8bc537162110d429d989f
Madelyn <mnelso12@nd.edu>
2016-09-01 14:54:32 -0400

    home nav bar navigates pages

---
+++ scripts/home.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($){
+	console.log('hey');
+
+	$("#homePlayersButton").click(function(){
+		console.log("players clicked");
+
+	});
+	
+	$("#homeStatsButton").click(function(){
+		console.log("stats clicked");
+	});
+	
+	$("#homeFunButton").click(function(){
+		console.log("fun clicked");
+	});
+	
+	$("#homeSeeUsButton").click(function(){
+		console.log("see us clicked");
+	});
+});
+
+
